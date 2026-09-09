127d3c1cf86e6d46aa4774edb63e0b6e1dee9fd3
code philospher <sharmashivraj506@gmail.com>
2026-09-09 08:52:24 +0530

    edit praxctice line in vollege

--- practice.c
+++ practice.c
@@ -34,16 +34,41 @@ int main() {
     //     printf("%d is greater than %d\n",a,b);
     // else
     //     printf("%d is greater than %d\n",b,a);  
-    printf("check which number is greater in 3 no.\n");
-    int a,b,c;
-    printf("Enter three numbers: ");
-    scanf("%d %d %d", &a, &b, &c);
-    if(a>b && a>c)
-        printf("%d is the greatest number.\n", a);
-    else if(b>a && b>c)
-        printf("%d is the greatest number.\n", b);
-    else
-        printf("%d is the greatest number.\n", c);  
+    // printf("check which number is greater in 3 no.\n");
+    // int a,b,c;
+    // printf("Enter three numbers: ");
+    // scanf("%d %d %d", &a, &b, &c);
+    // if(a>b && a>c)
+    //     printf("%d is the greatest number.\n", a);
+    // else if(b>a && b>c)
+    //     printf("%d is the greatest number.\n", b);
+    // else
+    //     printf("%d is the greatest number.\n", c); 
+    // int a;
+    // float b;
+    // char c;
+    // printf("Enter an age: ");
+    // scanf("%d", &a);
+    // printf("Enter your marks: ");
+    // scanf("%f", &b);
+    // printf("Enter your grade: ");
+    // scanf(" %c", &c);
+    // printf("Age: %d\n", a);
+    // printf("Marks: %.2f\n", b);
+    // printf("Grade: %c\n", c);   
+    printf("Area of circle and circumference of circle\n");
+    int radius;
+    float area, circumference;
+    float pi = 3.14159;
+
+    printf("Enter the radius of the circle: ");
+    scanf("%d", &radius);
+
+    area = pi * radius * radius;
+    circumference = 2 * pi * radius;
+
+    printf("Area of the circle: %.2f\n", area);
+    printf("Circumference of the circle: %.2f\n", circumference);       
 
     return 0;
 }
